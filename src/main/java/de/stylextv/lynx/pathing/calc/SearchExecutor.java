3e0e03cb9127d2aae194094381af946cc6082213
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-09-01 16:28:50 +0200

    Update SearchExecutor.java

--- src/main/java/de/stylextv/lynx/pathing/calc/SearchExecutor.java
+++ src/main/java/de/stylextv/lynx/pathing/calc/SearchExecutor.java
@@ -11,7 +11,7 @@ public class SearchExecutor {
 	
 	private static final long PLAN_AHEAD_TIMEOUT = 4000;
 	
-	private static final long CALCULATION_TIME_BUFFER = 5000;
+	private static final long CALCULATION_TIME_BUFFER = 6000;
 	
 	private static final long SLEEP_TIME = 500;
 	
