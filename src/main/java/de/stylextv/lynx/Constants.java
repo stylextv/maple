71f82bb0b2fbbfd5913e821f37e2ecbdd04744f1
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-09-12 14:46:00 +0200

    Increased version

--- src/main/java/de/stylextv/lynx/Constants.java
+++ src/main/java/de/stylextv/lynx/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	
 	public static final String COLORED_NAME = NAME_COLOR + NAME;
 	
-	public static final String VERSION = "v1.3.4";
+	public static final String VERSION = "v1.4.0";
 	
 	public static final String AUTHOR = "StylexTV";
 	
