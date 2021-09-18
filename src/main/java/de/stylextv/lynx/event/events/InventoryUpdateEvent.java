8a57a7a10cbee550997ce946926e325277678710
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-09-18 09:35:13 +0200

    Added InventoryUpdateEvent

---
+++ src/main/java/de/stylextv/lynx/event/events/InventoryUpdateEvent.java
@@ -0,0 +1,25 @@
+package de.stylextv.lynx.event.events;
+
+import de.stylextv.lynx.event.Event;
+import de.stylextv.lynx.event.EventListener;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+
+public class InventoryUpdateEvent extends Event {
+	
+	private DefaultedList<ItemStack> list;
+	
+	public InventoryUpdateEvent(DefaultedList<ItemStack> list) {
+		this.list = list;
+	}
+	
+	public DefaultedList<ItemStack> getList() {
+		return list;
+	}
+	
+	@Override
+	public void callListener(EventListener l) {
+		l.onInventoryUpdate(this);
+	}
+	
+}
---
+++ src/main/java/de/stylextv/lynx/event/listeners/InventoryListener.java
@@ -0,0 +1,22 @@
+package de.stylextv.lynx.event.listeners;
+
+import de.stylextv.lynx.context.PlayerContext;
+import de.stylextv.lynx.event.EventListener;
+import de.stylextv.lynx.event.events.InventoryUpdateEvent;
+import de.stylextv.lynx.gui.ToolSet;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+
+public class InventoryListener implements EventListener {
+	
+	@Override
+	public void onInventoryUpdate(InventoryUpdateEvent event) {
+		DefaultedList<ItemStack> list = event.getList();
+		
+		PlayerInventory inv = PlayerContext.inventory();
+		
+		if(list.equals(inv.main)) ToolSet.update();
+	}
+	
+}
