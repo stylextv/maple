f92bad601fe37f39a71d41380ce0b5bb9607e81e
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-09-18 18:07:25 +0200

    Update DefaultedListMixin.java

--- src/main/java/de/stylextv/lynx/mixin/DefaultedListMixin.java
+++ src/main/java/de/stylextv/lynx/mixin/DefaultedListMixin.java
@@ -3,20 +3,24 @@ package de.stylextv.lynx.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import de.stylextv.lynx.event.EventBus;
+import de.stylextv.lynx.event.events.InventoryUpdateEvent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
 
 @Mixin(DefaultedList.class)
 public class DefaultedListMixin<T> {
 	
-	@Inject(method = "set(ILjava/lang/Objec;)V", at = @At("TAIL"))
-	private void set(int index, T element, CallbackInfo info) {
-		System.out.println("inv update");
-		System.out.println(this);
+	@SuppressWarnings("unchecked")
+	@Inject(method = "set(ILjava/lang/Object;)V", at = @At("TAIL"))
+	private void set(int index, T element, CallbackInfoReturnable<T> info) {
+		if(!(element instanceof ItemStack)) return;
 		
-//		EventBus.onEvent(new InventoryUpdateEvent());
+		DefaultedList<ItemStack> list = (DefaultedList<ItemStack>)(Object) this;
+		
+		EventBus.onEvent(new InventoryUpdateEvent(list));
 	}
 	
 }
