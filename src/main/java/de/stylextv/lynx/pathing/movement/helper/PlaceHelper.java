c3397fac428ea9ab9f2176b322bfe85af31ef7d0
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-09-19 10:35:57 +0200

    Update PlaceHelper.java

--- src/main/java/de/stylextv/lynx/pathing/movement/helper/PlaceHelper.java
+++ src/main/java/de/stylextv/lynx/pathing/movement/helper/PlaceHelper.java
@@ -23,7 +23,7 @@ public class PlaceHelper extends TargetHelper {
 		if(!hasTarget(x, y, z)) addTarget(x, y, z);
 	}
 	
-	// TODO check if canPlaceAt 
+	// TODO check if canPlaceAt
 	@Override
 	public double cost() {
 		int l = getTargets().size();
