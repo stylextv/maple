689ed11a2a782e5fa42b36f03d7a5be08f64d137
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-10-14 17:48:39 +0200

    Incremented build number

--- src/main/java/de/stylextv/maple/Constants.java
+++ src/main/java/de/stylextv/maple/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	
 	public static final String COLORED_NAME = NAME_COLOR + NAME;
 	
-	public static final String VERSION = "v0.1.0+build.2";
+	public static final String VERSION = "v0.1.0+build.3";
 	
 	public static final String AUTHOR = "StylexTV";
 	
