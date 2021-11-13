70d3ad9be7cb8b2ec40e8bd98fffd6f5cbbd039b
StylexTV <56502218+StylexTV@users.noreply.github.com>
2021-11-13 23:03:03 +0100

    Update FileAccess.java

--- src/main/java/de/stylextv/maple/io/FileAccess.java
+++ src/main/java/de/stylextv/maple/io/FileAccess.java
@@ -13,9 +13,9 @@ import java.util.zip.InflaterInputStream;
 
 public class FileAccess {
 	
-	private static final int FILE_VERSION = 0x9BA2F52;
-	
 	private static final int COMPRESSION_LEVEL = Deflater.BEST_COMPRESSION;
+	
+	private static final int FILE_VERSION = 0x9BA2F52;
 	
 	private File file;
 	
